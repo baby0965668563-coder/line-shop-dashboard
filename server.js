162da26c5b57cef8322369dc70a31992d4f227a9
baby0965668563-coder <baby0965668563@gmail.com>
2026-04-26 23:42:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+// 讓 public 裡的東西可以被讀到
 app.use(express.static('public'));
+
+// 👉 加這段（關鍵）
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(3000, () => {
   console.log('Server running');
